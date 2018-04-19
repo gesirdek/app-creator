8a34a414e0648f18dffb833e35dd33a317334dd7
Muhittin Tan <muhittin.tan@boun.edu.tr>
2018-04-19 15:51:59 +0300

    bug fix

--- publish/views/admin.blade.php
+++ publish/views/admin.blade.php
@@ -4,7 +4,6 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ URL::asset('css/app.css') }}" />
         <link rel="stylesheet" href="{{ URL::asset('css/vuetify.css') }}" />
         <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
               rel="stylesheet">
