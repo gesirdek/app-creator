23ed9c3d46549d354208dd1d7e50b3d3279b3e6c
Muhittin Tan <muhittin.tan@boun.edu.tr>
2018-07-27 10:58:09 +0300

    translation prepared

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,37 +1,5 @@
 <?php
-Route::group(['prefix'=>'gesirdek'], function()
-{
-    /**
-     * Get component translations for vue
-     * @param $locale
-     * @return array
-     */
-    Route::get('get-component-translations/{locale}', function ($locale) {
-        $return = [];
-        $escapes = ['.','..'];
-        foreach (scandir(resource_path('lang/'.$locale)) as $component){
-            if(in_array($component,$escapes) || substr($component,0,10)!=='Component-')
-                continue;
-            $cname = substr($component,0,-4);
-            $return[substr($cname,10)]= trans($cname,[],$locale);
-        }
-        return $return;
-    });
-
-    /**
-     * Change language
-     * @param $lang
-     * @return int
-     */
-    Route::post('set-language/{lang}', function ($lang) {
-        if (\Illuminate\Support\Facades\Auth::check()) {
-            $user = User::find(\Illuminate\Support\Facades\Auth::user()->id);
-            $user->lang = $lang;
-            $user->save();
-        } else {
-            setcookie('locale', $lang);
-        }
-        return 1;
-
-    });
+Route::prefix('gesirdek')->name('gesirdek.')->group(function () {
+    Route::get('get-component-translations/{locale}',"\Gesirdek\Http\Controllers\ApiController@getComponentTranslations")->name('get-component-translations');
+    Route::post('set-language/{lang}','\Gesirdek\Http\Controllers\ApiController@setLanguage')->name('set-language');
 });
